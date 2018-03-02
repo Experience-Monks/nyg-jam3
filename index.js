cb932f452ada3bf769b48925835022219a20b4d0
Iran Reyes <iran.reyes@jam3.com>
2018-03-02 15:32:02 -0500

    Update .nvmrc automatically after install

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const nyg = require('nyg');
+const exec = require('child_process').exec;
 
+// Generator configuration
 const globs = [{ base: 'templates/' }];
 
-const generator = nyg(null, globs).run();
+const generator = nyg(null, globs)
+  .on('postinstall', onPostInstall)
+  .run();
+
+/**
+ * Post Install event
+ */
+function onPostInstall() {
+  var done = generator.async();
+  updateNvmVersion(done);
+}
+
+/**
+ * Update NVM lock version with the current user sections
+ *
+ * @param {any} done - Generator asynchronous callback
+ */
+function updateNvmVersion(done) {
+  exec(`node -v > ${generator.cwd}/.nvmrc`, function(err, stdout, stderr) {
+    done();
+  });
+}
