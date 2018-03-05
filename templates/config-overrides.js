828960e8672a25a62ad78ab59abc456a4f525edf
Iran Reyes <iran.reyes@jam3.com>
2018-03-05 16:15:48 -0500

    Add bundle size analizer to cra-rewire

--- templates/config-overrides.js
+++ templates/config-overrides.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
-const DEBUG = true;
+const Visualizer = require('webpack-visualizer-plugin');
+
+const DEBUG = false;
 
 module.exports = function override(config, env) {
   DEBUG && fs.writeFile('default-cra-rewrite-config.json', JSON.stringify(config));
+
+  // Bundle Analizer - Visualizer
+  DEBUG && config.plugins.push(new Visualizer({ filename: './public/bundle-size-analizer.html' }));
 
   DEBUG && fs.writeFile('final-cra-rewrite-config.json', JSON.stringify(config));
   return config;
