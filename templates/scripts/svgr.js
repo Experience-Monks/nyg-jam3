149b19ea296b003b023b04a8db6ea74b93d91c14
DonghyukJacobJang <donghyuk0117@gmail.com>
2018-04-03 16:27:23 -0400

    refactor svgr script

--- templates/scripts/svgr.js
+++ templates/scripts/svgr.js
@@ -1,128 +1,200 @@
+/**
+ * List of node script arguments
+ *
+ * @argument -r (Root dirotory, SvgComponents by default)
+ * @argument -noSubDir (No sub-directory, false by default)
+ * @argument -index-js (Generate [svg file name].js by default.
+ *                      If noSubDir is true, noIndexJs will be true)
+ */
+
 const fs = require('fs');
 const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
-const mkdirp = require('mkdirp');
 const svgr = require('svgr').default;
+const chalk = require('chalk');
 
-console.log(argv);
 /**
- * NOTE : argument list
- *        -r [dir-name]     - root dirotory, SvgComponents by default
- *        --noSubDir        - no sub-directory, false by default
- *        --component-js    - generate [svg file name].js, index.js by default
- *                          - if noSubDir is true, noIndexJs will be true
+ * Svgr library options
+ *
+ * @property prettier (Use Prettier to format JavaScript code output)
+ * @property singleQuote (Use single quotes instead of double quotes)
+ * @property dimensions (Remove width and height from root SVG tag)
+ * @property presicion (Set number of digits in the fractional part)
+ * @property ref (To hook into the ref of the svg components)
+ *
+ * See more details at https://github.com/smooth-code/svgr
  */
-const rootDir = argv.r || 'SvgComponents';
-const noSubDir = argv.noSubDir || false;
-const noIndexJs = noSubDir || argv['component-js'] || false;
-console.log(`-------------------SCRIPT CONFIG--------------------
-rootDir   : ${rootDir}
-noSubDir  : ${noSubDir}
-noIndexJs : ${noIndexJs}
-----------------------------------------------------`);
-
-// svgr options
 const SVGR_OPTS = {
   prettier: true,
   singleQuote: true,
   dimensions: false,
   precision: 3,
-  ref: true
+  ref: false
 };
 
-const COMPONENT_PATH = path.resolve(__dirname, [`../src/components/${rootDir}`].join(''));
+/**
+ * Global variables
+ *
+ * @const {boolean} noSubDir (wheather using sub directory structure or not)
+ * @const {boolean} useIndexJs (determin type of file name for Svg components)
+ * @const {string} parentDir (the most parent directory)
+ * @const {string} COMPONENT_PATH (Svg component directory path)
+ * @const {array} files (An array of file path)
+ * @const {array} directories (An array of directory path)
+ */
+const noSubDir = argv.noSubDir || false;
+const useIndexJs = noSubDir || argv['index-js'] || false;
+const parentDir = argv.r || 'SvgComponents';
+const COMPONENT_PATH = path.resolve(__dirname, [`../src/components/${parentDir}`].join(''));
 const files = [];
 const directories = [];
 
-// check root directory
-fs.stat(COMPONENT_PATH, (err, stat) => {
-  if (err) {
-    mkdirp(COMPONENT_PATH, err => {
-      if (err) {
-        console.log(`[Error] fail to create root directory.`);
-        console.log(err);
-      } else {
-        console.log(`[Report] new root directory has been created.`);
-        getAllFiles(COMPONENT_PATH);
-        cleanUp();
-        generateSvgComponents();
-      }
-    });
-  } else {
-    console.log(`[Report] root directory ${rootDir} is already existed.`);
-    getAllFiles(COMPONENT_PATH);
-    cleanUp();
-    generateSvgComponents();
+/**
+ * Custom asyncForEach function
+ *
+ * @param {array} array
+ * @param {function} callback
+ */
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
   }
-});
+}
 
+/**
+ * Promise wait
+ *
+ * @param {number} ms (millisecond)
+ */
+const waitFor = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * Capitalize the first letter of string
+ *
+ * @param {string} string
+ * @returns {string} (parsed string)
+ */
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-// get all files and sub directories in SvgComponents directory
-function getAllFiles(dir) {
-  fs.readdirSync(dir).forEach(file => {
-    const name = path.join(dir, file);
+/**
+ * Check directory path. Create directory if not existed
+ *
+ * @param {string} directory (directory path)
+ */
+function checkDirectory(directory) {
+  try {
+    fs.statSync(directory);
+    console.log(`[checkDirectory] path is existed : ${directory}`);
+  } catch (error) {
+    try {
+      fs.mkdirSync(directory);
+      console.log(`[checkDirectory] new directory has been created : ${directory}`);
+    } catch (error) {
+      console.error(chalk.red(`[Error] fail to create a directory : ${directory}`));
+      console.error(chalk.red(error));
+      process.exit(1);
+    }
+  }
+}
+
+/**
+ * Recursively collect all file and sub directory path in SvgComponents directory
+ *
+ * @param {string} dir (directory path)
+ * @returns {array} (an array of files and directories)
+ */
+function getAllFiles(directory) {
+  fs.readdirSync(directory).forEach(file => {
+    const name = path.join(directory, file);
+
     if (fs.statSync(name).isDirectory()) {
       directories.push(name);
       getAllFiles(name);
-    }
-
-    if (fs.statSync(name).isFile()) {
+    } else if (fs.statSync(name).isFile()) {
       files.push(name);
+    }
+  });
+
+  // filter files if svg and sort array files first and then adding directories
+  return files.filter(file => path.extname(file) !== '.svg').concat(directories);
+}
+
+/**
+ * Clean up files
+ *
+ * @param {array} files (an array of files and directories)
+ */
+async function cleanFiles(allFiles) {
+  console.log(`\n\n------------------Cleaning existing files--------------------\n`);
+  await asyncForEach(allFiles, async file => {
+    await waitFor();
+    if (fs.statSync(file).isDirectory()) {
+      try {
+        fs.rmdirSync(file);
+        console.log(`[DELETE] directory ${file}`);
+      } catch (error) {
+        console.error(chalk.red(`[Error] fail to delete a directory : ${file}`));
+        console.error(`${error}`);
+      }
+    } else if (fs.statSync(file).isFile()) {
+      try {
+        fs.unlinkSync(file);
+        console.log(`[DELETE] file ${file}`);
+      } catch (error) {
+        console.error(chalk.red(`[Error] fail to delete a file : ${file}`));
+        console.error(`${error}`);
+      }
     }
   });
 }
 
-// clean up SvgComponent directories if not svg
-function cleanUp() {
-  files
-    .filter(file => path.extname(file) !== '.svg')
-    .concat(directories)
-    .forEach(file => {
-      if (path.extname(file) === '') {
-        fs.rmdir(file, err => {
-          if (err) {
-            console.log(`${err}`);
-          }
-          console.log(`[DELETE] directory ${file}`);
-        });
-      } else {
-        fs.unlink(file, err => {
-          if (err) {
-            console.log(`${err}`);
-          }
-          console.log(`[DELETE] file ${file}`);
-        });
-      }
-    });
-}
-
-// generate svg component by using svgr
-function generateSvgComponents() {
-  files.filter(file => path.extname(file) === '.svg').forEach(file => {
+/**
+ * Generate svg component
+ *
+ * @param {array} svgs (an array of svgs in the component directory)
+ */
+function generateSvgComponents(svgs) {
+  console.log(`\n\n-------------------Generate SVG COMPONENT--------------------\n`);
+  svgs.forEach(file => {
     fs.readFile(file, 'utf8', (err, content) => {
       var filename = file.replace(/^.*[\\/]/, '');
       const outputFilename = capitalizeFirstLetter(filename.split('.')[0]);
-
-      // make directory
       const filePath = path.join(COMPONENT_PATH, noSubDir ? '' : outputFilename);
-      mkdirp(filePath, err => {
-        if (err) {
-          console.log(`[error : mkdirp] ${err}`);
-          return;
-        }
 
-        // convert svg to react component
+      try {
+        // make directory
+        fs.mkdirSync(filePath);
+
+        // convert svg to React component
         svgr(content, { ...SVGR_OPTS, componentName: outputFilename }).then(svgr_content => {
-          const finalFile = path.join(filePath, noIndexJs ? outputFilename + '.js' : 'index.js');
+          const finalFile = path.join(filePath, useIndexJs ? 'index.js' : outputFilename + '.js');
           fs.writeFile(finalFile, svgr_content, err => {
             if (err) return;
             console.log(`[Svg Component] ${finalFile} Successful!`);
           });
         });
-      });
+      } catch (error) {
+        console.error(chalk.red(`[Error] fail to create a directory ${file}`));
+        console.error(chalk.red(error));
+      }
     });
   });
 }
+
+/**
+ * Start script
+ */
+console.log(`-------------------SCRIPT CONFIG--------------------
+parentDir   : ${parentDir}
+noSubDir  : ${noSubDir}
+useIndexJs : ${useIndexJs}
+----------------------------------------------------\n`);
+
+checkDirectory(COMPONENT_PATH);
+const allFiles = getAllFiles(COMPONENT_PATH);
+
+cleanFiles(allFiles).then(() => {
+  generateSvgComponents(files.filter(file => path.extname(file) === '.svg'));
+});
--- templates/src/components/SvgComponents/Close/Close.js
+++ templates/src/components/SvgComponents/Close/Close.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Close = ({ svgRef, ...props }) => (
-  <svg viewBox="0 0 88 88" {...props} ref={svgRef}>
+const Close = props => (
+  <svg viewBox="0 0 88 88" {...props}>
     <title>Dismiss</title>
     <g stroke="#063855" strokeWidth={2} fill="none" fillRule="evenodd" strokeLinecap="square">
       <path d="M51 37L37 51M51 51L37 37" />
