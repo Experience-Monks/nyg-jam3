17c82f4fcbd2fe795209b7de99ff1b5c45a94693
namniak <vnamnyak@gmail.com>
2018-04-30 17:17:01 -0400

    added tabindex + event handlers

--- templates/src/components/BaseLink/BaseLink.js
+++ templates/src/components/BaseLink/BaseLink.js
@@ -7,22 +7,26 @@ import './BaseLink.css';
 
 import checkProps from '../../util/check-props';
 
+const excludes = ['children', 'download', 'target', 'rel', 'link'];
+
 const BaseLink = props => {
-  const { title, onClick } = props;
   const Tag =
     props.link.includes('://') || props.link.includes('tel:') || props.link.includes('mailto:') || props.download
       ? 'a'
       : Link;
 
-  let componentProps = { className: classnames('BaseLink', props.className), title, onClick };
+  // clean props
+  let componentProps = Object.keys(props).reduce(
+    (acc, key) => ([...excludes].includes(key) ? acc : { ...acc, [key]: props[key] }),
+    {}
+  );
 
   if (Tag === 'a') {
     componentProps.href = props.link;
+    componentProps.download = props.download;
 
-    if (props.download) {
-      componentProps.download = props.download;
-    } else if (!props.link.includes('tel:') && !props.link.includes('mailto:')) {
-      // external link
+    // set external link attributes
+    if (!props.link.includes('tel:') && !props.link.includes('mailto:') && !props.download) {
       componentProps.target = props.target;
       if (props.target === '_blank') {
         componentProps.rel = props.rel || 'noopener';
@@ -33,7 +37,11 @@ const BaseLink = props => {
     componentProps.to = props.link;
   }
 
-  return <Tag {...componentProps}>{props.children}</Tag>;
+  return (
+    <Tag className={classnames('BaseLink', props.className)} {...componentProps}>
+      {props.children}
+    </Tag>
+  );
 };
 
 BaseLink.propTypes = checkProps({
@@ -41,10 +49,23 @@ BaseLink.propTypes = checkProps({
   rel: PropTypes.string,
   link: PropTypes.string,
   target: PropTypes.oneOf(['_blank', '_self']),
-  onClick: PropTypes.func,
+  tabIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   download: PropTypes.string,
   title: PropTypes.string,
-  children: PropTypes.node
+  children: PropTypes.node,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onKeyUp: PropTypes.func,
+  onMouseMove: PropTypes.func,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
+  onMouseUp: PropTypes.func,
+  onMouseDown: PropTypes.func,
+  onTouchEnd: PropTypes.func,
+  onTouchMove: PropTypes.func,
+  onTouchStart: PropTypes.func,
+  onClick: PropTypes.func
 });
 
 BaseLink.defaultProps = {
