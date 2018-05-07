4593bb7de457bd58cab0d1cdc9c952daf302ce5c
sdantuoni <santiago.dantuoni@jam3.com>
2018-05-07 16:24:38 -0300

    Add commitlint

---
+++ templates/commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'fix', 'feature', 'issue', 'perf', 'refactor', 'revert', 'style', 'test']
+    ]
+  }
+};
