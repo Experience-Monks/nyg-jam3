93d0c18feca12f5c6015fcbfbd1c2c610d4360aa
Benson Wong <wongbsn@gmail.com>
2018-06-15 10:25:57 -0400

    Appending newline to built layout.scss to follow prettier style convention

--- templates/scripts/build-layout-data.js
+++ templates/scripts/build-layout-data.js
@@ -2,20 +2,31 @@ const fs = require('fs');
 const path = require('path');
 const jsonSass = require('json-sass-vars');
 
-const FILENAME = 'layout';
-const PREFIX = 'layout';
+const SASS_VAR_PREFIX = 'layout';
+const SRC_FILENAME = 'layout';
+const SRC_URL = path.resolve(__dirname, `../src/data/${SRC_FILENAME}.json`);
+const DEST_URL = path.resolve(__dirname, `../src/style/${SRC_FILENAME}.scss`);
 
 function buildLayoutData() {
+  const writeStream = fs.createWriteStream(DEST_URL);
+
+  writeStream.on('close', () => {
+    // Appending extra newline due to prettier style convention
+    fs.appendFileSync(DEST_URL, '\r\n', err => {
+      if (err) throw err;
+    });
+  });
+
   return fs
-    .createReadStream(path.resolve(__dirname, `../src/data/${FILENAME}.json`))
+    .createReadStream(SRC_URL)
     .pipe(
       jsonSass({
-        prefix: `$${PREFIX}: `
+        prefix: `$${SASS_VAR_PREFIX}: `
       }).on('error', function() {
         console.error('Error reading layout.json');
       })
     )
-    .pipe(fs.createWriteStream(path.resolve(__dirname, `../src/style/${FILENAME}.scss`)));
+    .pipe(writeStream);
 }
 
 if (module.parent) {
