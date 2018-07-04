2d8e732411810e7d0cf035bcdb2555304e40dd28
Nick <nick@jam3.com>
2018-07-04 15:02:00 -0400

    Added error reporting to facebook script

--- templates/scripts/facebook.js
+++ templates/scripts/facebook.js
@@ -43,32 +43,36 @@ if (!share) {
 }
 
 if (argv.url) {
-  var url;
-  if (argv.url.charAt(argv.url.length - 1) === '/') {
-    url = encodeURIComponent(argv.url);
+  if (process.env.FB_APP_ID && process.env.FB_APP_SECRET) {
+    var url;
+    if (argv.url.charAt(argv.url.length - 1) === '/') {
+      url = encodeURIComponent(argv.url);
+    } else {
+      url = encodeURIComponent(argv.url + '/');
+    }
+    const opts = {
+      client_id: process.env.FB_APP_ID,
+      client_secret: process.env.FB_APP_SECRET,
+      grant_type: 'client_credentials'
+    };
+    call(`/oauth/access_token?${querystring.stringify(opts)}`, function(data) {
+      var token = data.replace('access_token=', '');
+      call('/', 'id=' + url + '&scrape=true&access_token=' + token, function(data) {
+        console.log('\x1b[32m CLEARED:', decodeURIComponent(url), '\x1b[0m');
+      });
+      Object.keys(share).forEach(function(cur, i) {
+        if (cur != 'default') {
+          setTimeout(function() {
+            call('/', 'id=' + url + encodeURIComponent(cur) + '&scrape=true&access_token=' + token, function(data) {
+              console.log('\x1b[32m CLEARED:', decodeURIComponent(url) + cur, '\x1b[0m');
+            });
+          }, (i + 1) * 500);
+        }
+      });
+    });
   } else {
-    url = encodeURIComponent(argv.url + '/');
+    console.log('\x1b[31m Please define a valid FB_APP_ID and FB_APP_SECRET in your environment variables\x1b[0m');
   }
-  const opts = {
-    client_id: process.env.FB_APP_ID,
-    client_secret: process.env.FB_APP_SECRET,
-    grant_type: 'client_credentials'
-  };
-  call(`/oauth/access_token?${querystring.stringify(opts)}`, function(data) {
-    var token = data.replace('access_token=', '');
-    call('/', 'id=' + url + '&scrape=true&access_token=' + token, function(data) {
-      console.log('\x1b[32m CLEARED:', decodeURIComponent(url), '\x1b[0m');
-    });
-    Object.keys(share).forEach(function(cur, i) {
-      if (cur != 'default') {
-        setTimeout(function() {
-          call('/', 'id=' + url + encodeURIComponent(cur) + '&scrape=true&access_token=' + token, function(data) {
-            console.log('\x1b[32m CLEARED:', decodeURIComponent(url) + cur, '\x1b[0m');
-          });
-        }, (i + 1) * 500);
-      }
-    });
-  });
 } else {
   console.log('\x1b[31m Please provide a url in this format: npm run facebook -- --url=[YOUR URL]\x1b[0m');
 }
