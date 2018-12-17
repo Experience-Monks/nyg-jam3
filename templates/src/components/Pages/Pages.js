af25664c53b551a2c5cfd22f3b88dfc0e98ceb92
DonghyukJacobJang <donghyuk0117@gmail.com>
2018-12-17 16:35:54 -0500

    Replace react-loadable with lazy and Suspense

--- templates/src/components/Pages/Pages.js
+++ templates/src/components/Pages/Pages.js
@@ -1,19 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, lazy, Suspense } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { TransitionGroup, Transition } from 'react-transition-group';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import checkProps from '../../util/check-props';
-import routeKeys from '../../routes/keys';
-import { getTransitionDuration } from '../../data/pages-transitions';
 import './Pages.scss';
 
-import {
-  AsyncLanding as Landing,
-  AsyncAbout as About,
-  AsyncNotFound as NotFound
-} from '../../util/async-section-handler';
+import checkProps from '../../util/check-props';
+import { noop } from '../../util/basic-functions';
+import routeKeys from '../../routes/keys';
+import { getTransitionDuration } from '../../data/pages-transitions';
+
+const Landing = lazy(() => import('../../pages/Landing/Landing'));
+const About = lazy(() => import('../../pages/About/About'));
+const NotFound = lazy(() => import('../../pages/NotFound/NotFound'));
 
 const Pages = ({ location, ...props }) => {
   return (
@@ -22,8 +22,24 @@ const Pages = ({ location, ...props }) => {
         <Transition appear key={location.pathname} timeout={getTransitionDuration(location.pathname)}>
           {state => (
             <Switch location={location}>
-              <Route exact path={routeKeys.Landing} render={() => <Landing transitionState={state} />} />
-              <Route exact path={routeKeys.About} render={() => <About transitionState={state} />} />
+              <Route
+                exact
+                path={routeKeys.Landing}
+                component={
+                  <Suspense fallback={noop}>
+                    <Landing transitionState={state} />
+                  </Suspense>
+                }
+              />
+              <Route
+                exact
+                path={routeKeys.About}
+                component={
+                  <Suspense fallback={noop}>
+                    <About transitionState={state} />
+                  </Suspense>
+                }
+              />
               <Route component={NotFound} />
             </Switch>
           )}
