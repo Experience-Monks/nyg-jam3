a55f798f3338e65645125691a668342207178ae1
Jacob Jang <donghyuk0117@gmail.com>
2019-01-03 12:24:25 -0500

    Fix text run

--- templates/src/util/layout.js
+++ templates/src/util/layout.js
@@ -3,6 +3,13 @@ import { medium as MEDIUM, large as LARGE } from '../data/layout.json';
 export const MEDIUM_MEDIA_QUERY = `(min-width: ${MEDIUM}px)`;
 export const LARGE_MEDIA_QUERY = `(min-width: ${LARGE}px)`;
 
+// this code below does particularly for fixing jest text script
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return { matches: false, addListener: function() {}, removeListener: function() {} };
+  };
+
 const MEDIUM_MATCH_MEDIA = window.matchMedia(MEDIUM_MEDIA_QUERY);
 const LARGE_MATCH_MEDIA = window.matchMedia(LARGE_MEDIA_QUERY);
 
