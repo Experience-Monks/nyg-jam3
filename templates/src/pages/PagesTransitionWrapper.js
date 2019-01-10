391ef94d3e9d8ba613a516be123bbd6f68819abf
namniak <vnamnyak@gmail.com>
2019-01-10 17:35:21 -0500

    fixing transition and UI positioning

--- templates/src/pages/PagesTransitionWrapper.js
+++ templates/src/pages/PagesTransitionWrapper.js
@@ -1,5 +1,7 @@
 import noop from 'no-op';
+import { findDOMNode } from 'react-dom';
 
+import { wait } from '../util/basic-functions';
 import { getExitTransitionDuration } from '../data/pages-transitions';
 
 const transitionStates = {
@@ -10,8 +12,17 @@ const transitionStates = {
 };
 
 const PagesTransitionWrapper = Class => {
-  const handleEnterTransition = (previousRoute, onEnter = noop, onAppear = noop) => {
-    previousRoute ? onEnter(getExitTransitionDuration(previousRoute) || 0) : onAppear();
+  let container;
+  const handleEnterTransition = async (previousRoute, onEnter = noop, onAppear = noop) => {
+    if (previousRoute) {
+      const transitionDuration = getExitTransitionDuration(previousRoute) || 0;
+      onEnter(transitionDuration);
+      await wait(transitionDuration);
+      container.style.display = '';
+    } else {
+      container.style.display = '';
+      onAppear();
+    }
   };
 
   const handleLeaveTransition = (onLeave = noop) => {
@@ -21,6 +32,10 @@ const PagesTransitionWrapper = Class => {
   const componentDidMount = Class.prototype.componentDidMount;
   Class.prototype.componentDidMount = function() {
     componentDidMount && componentDidMount.call(this);
+
+    container = findDOMNode(this);
+    container.style.display = 'none';
+
     if (
       this.props.transitionState === transitionStates.entered ||
       this.props.transitionState === transitionStates.entering
