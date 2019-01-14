d708fbbf4599e3282b3c5c36f2fcda9776547582
Jacob Jang <donghyuk0117@gmail.com>
2019-01-14 16:37:17 -0500

    Test linters

--- templates/src/index.js
+++ templates/src/index.js
@@ -3,11 +3,11 @@ import 'normalize.css';
 import './util/unsupported';
 import './util/polyfills';
 import framework from './framework';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 import './style/global.scss';
 
 if (process.env.NODE_ENV !== 'production' && window.location.href.indexOf('?nostat') === -1) require('./util/stats')();
 
 framework();
-serviceWorker.register();
+// serviceWorker.register();
