1b09cd96bf98147361c4a649fec20f05af65f7ec
Alejandro Mesa <alejandro.mesa1991@gmail.com>
2019-01-18 17:26:27 -0500

    Add commented out staggerFrom and staggerFromTo

--- templates/src/util/gsap-animate.js
+++ templates/src/util/gsap-animate.js
@@ -9,16 +9,43 @@ import { TweenLite, CSSPlugin } from 'gsap';
 
 const animate = require('gsap-promisify')(Promise, TweenLite);
 
-animate.staggerTo = function(els, duration, props, delay) {
+animate.staggerTo = function (els, duration, props, staggerDelay) {
   return Promise.all(
     els.map((el, i) =>
       animate.to(el, duration, {
         ...props,
-        delay: (props.delay || 0) + delay * i
+        delay: (props.delay || 0) + staggerDelay * i
       })
     )
   );
 };
+
+// animate.staggerFrom = function (els, duration, props, staggerDelay) {
+//   return Promise.all(
+//     els.map((el, i) =>
+//       animate.from(el, duration, {
+//         ...props,
+//         delay: (props.delay || 0) + staggerDelay * i
+//       })
+//     )
+//   );
+// };
+
+// animate.staggerFromTo = function (els, duration, initialProps, finalProps, staggerDelay) {
+//   return Promise.all(
+//     els.map((el, i) =>
+//       animate.fromTo(
+//         el,
+//         duration,
+//         { ...initialProps },
+//         {
+//           ...finalProps,
+//           delay: (finalProps.delay || 0) + staggerDelay * i
+//         }
+//       )
+//     )
+//   );
+// };
 
 export const GSAP_PLUGINS = { CSSPlugin };
 export default animate;
