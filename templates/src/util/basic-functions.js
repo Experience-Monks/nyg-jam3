a7702b2ed4621f2e991698a40aeb4032a5bdda70
Iran Reyes <iran.reyes@jam3.com>
2019-02-04 23:11:58 -0500

    Add @jam3/wait

--- templates/src/util/basic-functions.js
+++ templates/src/util/basic-functions.js
@@ -1,11 +1,1 @@
-/**
- * Wait a specific amount of time and return a promise
- * Promisified setTimeout
- *
- * @export
- * @param {number} [ms=0]
- * @returns {Promise}
- */
-export function wait(ms = 0) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// Add small functions you use as utils here
