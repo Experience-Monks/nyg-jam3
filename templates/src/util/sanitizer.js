77fd36bf6e1383af4527f0087d8fa4c329f7277f
namniak <vnamnyak@gmail.com>
2019-03-12 16:14:50 -0400

    js docs

--- templates/src/util/sanitizer.js
+++ templates/src/util/sanitizer.js
@@ -1,16 +1,16 @@
 import dompurify from 'dompurify';
 
-/**
- * DOM Sanitizer to protect against untrust inputs and XSS attacks
- *
- * @param {string} [dirtyInput=''] - Input to sanitize
- */
-
 const config = {
   ADD_ATTR: ['target']
 };
 
-function sanitizer(dirtyInput, ...options) {
+/**
+ * DOM Sanitizer to protect against untrust inputs and XSS attacks
+ *
+ * @param {string} [dirtyInput=''] - Input to sanitize
+ * @param {object} [options={}] - Options to override default config
+ */
+function sanitizer(dirtyInput, options = {}) {
   return dompurify.sanitize(dirtyInput, { ...config, ...options });
 }
 
