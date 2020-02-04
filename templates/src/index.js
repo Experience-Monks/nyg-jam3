41b52e8081273eb1ece514e6324fb5a113732329
Iran Reyes Fleitas <iran.reyes@jam3.com>
2020-02-03 21:42:34 -0500

    Update index.js

--- templates/src/index.js
+++ templates/src/index.js
@@ -1,11 +1,10 @@
 import 'normalize.css';
+import './style/global.scss';
 
 import './util/unsupported';
 import './util/polyfills';
 import framework from './framework';
 import * as serviceWorker from './serviceWorker';
-
-import './style/global.scss';
 
 if (process.env.NODE_ENV !== 'production' && window.location.href.indexOf('?nostat') === -1) require('@jam3/stats')();
 
